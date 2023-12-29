4e0a7da43534fe5ff408d8e41a812e3efa9b2226
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2023-12-29 14:45:22 +0200

    Adding packages and some changes

--- client/src/App.js
+++ client/src/App.js
@@ -4,56 +4,10 @@ import './App.css';
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">My Blog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Letlhogonolo Chauke</a>
-            <time>2023-12-29 14:15</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Letlhogonolo Chauke</a>
-            <time>2023-12-29 14:15</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.</p>
-        </div>
-      </div>
-
-      <div className="post">
-        <div className="image">
-          <img src="https://techcrunch.com/wp-content/uploads/2022/12/lawnmower-Large.jpeg?w=1390&crop=1" alt=""/>
-        </div>
-        <div className="texts">
-          <h2>Full-house battery backup coming later this year</h2>
-          <p className="info">
-            <a className="author">Letlhogonolo Chauke</a>
-            <time>2023-12-29 14:15</time>
-          </p>
-          <p className="summary">Today at its special launch event, home backup power giant EcoFlow launched a flurry of new products, including a “Whole-Home Backup Power Solution.</p>
-        </div>
-      </div>
       
+      <Post />
+      <Post />
+      <Post />
     </main>
   );
 }
