38039fccf3fcb9a8cc8116ed8cc85fd6567c3f7c
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2024-01-03 07:26:20 +0200

    Adding functionalities to the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,21 @@
 
 import './App.css';
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+import Layout from "./Layout";
+import IndexPage from "./pages/IndexPage";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
   return (
-    <main>
-      
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<IndexPage />} />
+        <Route path={'/Login'} element={<LoginPage />} />
+      </Route>
+    </Routes>
+    
   );
 }
 
