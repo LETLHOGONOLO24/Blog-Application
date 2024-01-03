6581442c0f9674073e2e133ba4da6b4157798cc1
LETLHOGONOLO24 <pitroschauke24@gmail.com>
2024-01-03 09:58:53 +0200

    Connecting to the database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
-app.get('/', (req, res) => {
-    res.json('test ok');
+app.use(cors());
+app.use(express.json());
+
+app.post('/register', (req, res) => {
+    const {username,password} = req.body;
+    res.json({requestData:{username,password}});
 });
 
 app.listen(4000);
+
+//mongodb+srv://pitroschauke24:Menjukapoy123@cluster0.gtfqfot.mongodb.net/?retryWrites=true&w=majority
